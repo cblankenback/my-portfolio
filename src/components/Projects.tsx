757849e0449df08f859d4f740b03e7d82e1e41b1
cblankenback <blankenbackc@icloud.com>
2025-05-08 16:35:35 -0400

    v1.2

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Github, X, CheckCircle } from 'lucide-react';
+import { Github, X } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 import hackathonImg from "../assets/hackathon.png";
 import ossoImg from "../assets/osso.png";
